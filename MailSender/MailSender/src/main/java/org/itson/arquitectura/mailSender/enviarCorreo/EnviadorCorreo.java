b619623d712472eff6ecf77ffe412d99c12198f4
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2024-09-26 00:57:32 -0700

    Documentacion

--- MailSender/MailSender/src/main/java/org/itson/arquitectura/mailSender/enviarCorreo/EnviadorCorreo.java
+++ MailSender/MailSender/src/main/java/org/itson/arquitectura/mailSender/enviarCorreo/EnviadorCorreo.java
@@ -13,14 +13,23 @@ public class EnviadorCorreo {
 
     private IMecanismo mecanismo;
 
-    public EnviadorCorreo(IMecanismo mecanismo) {
-        this.mecanismo = mecanismo;
+    /**
+     * Constructor.
+     */
+    public EnviadorCorreo() {
+        this.mecanismo = new JavaMailApi();
     }
 
+    /**
+     * Permite enviar un correo con una configuración especifica.
+     *
+     * @param correo El correo a enviar.
+     * @param configuracion La configuración a usar para enviar el correo
+     */
     public void enviar(Correo correo, Configuracion configuracion) {
         if (correo.validar() && configuracion.validar()) {
             mecanismo.enviar(correo, configuracion);
         }
     }
-    
+
 }
--- MailSender/MailSender/src/main/java/org/itson/arquitectura/mailSender/enviarCorreo/IMecanismo.java
+++ MailSender/MailSender/src/main/java/org/itson/arquitectura/mailSender/enviarCorreo/IMecanismo.java
@@ -11,6 +11,12 @@ import datos.Correo;
  */
 public interface IMecanismo {
 
+    /**
+     * Permite enviar un correo con una configuración especifica.
+     *
+     * @param correo El correo a enviar
+     * @param configuracion La configuración a usar para enviar el correo
+     */
     public void enviar(Correo correo, Configuracion configuracion);
 
 }
